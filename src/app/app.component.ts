33786e45a041c79f0f851bf52103cb282ef69ada
Daniel Prieto <88695568+danobanano88@users.noreply.github.com>
2022-05-03 17:27:17 -0500

    creacion de animaciones de padre e hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { trigger, transition, query, group, style, animate, state, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('theParentAnimation', [
+      state('down', style({
+        transform: 'translateY( 100% ) translateZ( 0 )',
+      })),
+      state('up', style({
+        transform: 'translateY( 0% ) translateZ( 0 )',
+      })),      
+      transition('* <=> *', [
+        group([
+          query('@theChildAnimation', animateChild()),
+          animate('0.9s cubic-bezier(0.55, 0.31, 0.15, 0.93)'),
+        ]),
+      ]),
+    ]),
+    trigger('theChildAnimation', [
+      state('down', style({
+        transform: 'translateY( 100% ) translateZ( 0 )',
+      })),
+      state('up', style({
+        transform: 'translateY( 0% ) translateZ( 0 )',
+      })),      
+      transition('* <=> *', [
+        animate('0.9s cubic-bezier(0.55, 0.31, 0.15, 0.93)'),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'child-animations';
+  state = 'up';
+  toggleState() {
+    this.state = ( this.state === 'up' ? 'down' : 'up' );
+  }
 }
